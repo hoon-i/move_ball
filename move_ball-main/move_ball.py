34452aeb400a704445354cfc832b7275a1152505
taemin <taemin2271@hufs.ac.kr>
2025-05-16 11:06:37 +0900

    taemin update python file

--- move_ball-main/move_ball.py
+++ move_ball-main/move_ball.py
@@ -27,5 +27,9 @@ def main(stdscr):
             y += 1    
         elif key == ord('q'):
             break
+        elif key == curses.KEY_LEFT and x > 0:
+            x -= 1
+        elif key == curses.KEY_RIGHT and x < width - 1:
+            x += 1
 
 curses.wrapper(main)
